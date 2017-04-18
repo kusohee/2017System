f90a7ac1146280a03956327ac224495fbdfde225
kusohee <sohee618@hanmail.net>
2017-04-18 15:38:53 +0900

    자바 소스 추가
    자바파일을 추가했습니다.

---
+++ ch1/a1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class a1 {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("두 정수를 입력하시오.");
+				
+		int a= scanner.nextInt();
+		int	b= scanner.nextInt();
+		int sum;
+		int mul;
+		
+		sum=a+b;
+		mul=a*b;
+		
+		System.out.println(sum);
+		System.out.println(mul);
+		
+		
+		
+
+	}
+
+}
