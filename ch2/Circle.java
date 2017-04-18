267c5d376c770992bb82c51d9e3302b79f3b2439
kusohee <sohee618@hanmail.net>
2017-04-18 15:45:51 +0900

    자바 또 추가
    자바를 한번 더 추가

---
+++ ch2/Circle.java
@@ -0,0 +1,14 @@
+
+public class Circle {
+
+	public static void main(String[] args) {
+		double radious = 4.5;
+		double circleArea;
+		
+		circleArea=radious*radious*3.14;
+		
+		System.out.printf("반지름이 4.5인 원의 면적 : %f",circleArea);
+
+	}
+
+}
